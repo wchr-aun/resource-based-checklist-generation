c7cce19fb3ddd22f9d9fcd9d2220eb533a1ced2c
wchr-aun <wchr.aun@gmail.com>
2022-07-12 09:11:21 +0100

    feat(database): adjust database to the new structure

---
+++ database/init.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS datamodel, models, input_information_child_query, input_information_child, input_information_parent, components, templates;
+
+CREATE TABLE IF NOT EXISTS models (name VARCHAR UNIQUE NOT NULL, "desc" VARCHAR NOT NULL);
+
+CREATE TABLE IF NOT EXISTS datamodel (name VARCHAR NOT NULL REFERENCES models(name), "table" VARCHAR NOT NULL, field VARCHAR NOT NULL, PRIMARY KEY (name, "table", field));
+
+CREATE TABLE templates (id SERIAL UNIQUE, name VARCHAR NOT NULL, created TIMESTAMP NOT NULL, updated TIMESTAMP NOT NULL, process_name VARCHAR NOT NULL);
+
+CREATE TABLE components (id SERIAL UNIQUE, template_id INTEGER REFERENCES templates(id) ON DELETE CASCADE NOT NULL, input_dep VARCHAR, input_dep_field VARCHAR, output_dep VARCHAR, output_dep_field VARCHAR, "order" INTEGER NOT NULL, "name" VARCHAR NOT NULL, type VARCHAR NOT NULL, required BOOLEAN DEFAULT true, hide BOOLEAN DEFAULT false, validation VARCHAR, function VARCHAR, parent INTEGER REFERENCES components(id) ON DELETE CASCADE);
+
+CREATE TABLE input_information_parent (id SERIAL UNIQUE, name VARCHAR NOT NULL, "order" INTEGER NOT NULL, input_dep VARCHAR REFERENCES models(name), template_id INTEGER NOT NULL REFERENCES templates(id) ON DELETE CASCADE);
+
+CREATE TABLE input_information_child (id SERIAL UNIQUE, name VARCHAR NOT NULL, "order" INTEGER NOT NULL, input_dep_field VARCHAR, hide BOOLEAN DEFAULT false, parent_id INTEGER NOT NULL REFERENCES input_information_parent(id) ON DELETE CASCADE);
+
+CREATE TABLE input_information_child_query (id SERIAL UNIQUE, foreign_table VARCHAR NOT NULL, foreign_key VARCHAR NOT NULL, query_table VARCHAR NOT NULL, query_field VARCHAR NOT NULL, is_array BOOLEAN DEFAULT false, details_id INTEGER NOT NULL REFERENCES input_information_child(id) ON DELETE CASCADE);
--- database/payment.sql
+++ database/payment.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS datamodel, models, input_information_child_query, input_information_child, input_information_parent, components, templates, transactions, item_list, orders, customers, items;
+DROP TABLE IF EXISTS transactions, transaction_status, item_list, orders, customers, items;
 
 CREATE TABLE items (id SERIAL UNIQUE, name VARCHAR NOT NULL, description TEXT);
 INSERT INTO items (name, description) VALUES('item 1', 'desc');
@@ -11,24 +11,25 @@ INSERT INTO customers (name, surname, address) VALUES('Aun', 'Sirinaovakul', 'Wo
 CREATE TABLE orders (id SERIAL UNIQUE, customer_id INTEGER REFERENCES customers(id), total_price INTEGER);
 INSERT INTO orders (customer_id, total_price) VALUES(1, 140), (1, 30);
 
-CREATE TABLE item_list (itemlistid SERIAL UNIQUE, item_id INTEGER REFERENCES items(id), price INTEGER NOT NULL, quantity INTEGER NOT NULL, order_id INTEGER REFERENCES orders(id));
+CREATE TABLE item_list (order_id INTEGER REFERENCES orders(id), item_id INTEGER REFERENCES items(id), price INTEGER NOT NULL, quantity INTEGER NOT NULL, PRIMARY KEY(order_id, item_id));
 INSERT INTO item_list (item_id, price, quantity, order_id) VALUES(1, 10, 1, 1), (2, 20, 2, 1), (3, 30, 3, 1);
-INSERT INTO item_list (item_id, price, quantity, order_id) VALUES(1, 10, 3, 1);
+INSERT INTO item_list (item_id, price, quantity, order_id) VALUES(1, 10, 3, 2);
 
-CREATE TABLE transactions (id SERIAL UNIQUE, order_id INTEGER REFERENCES orders(id), payment_date TIMESTAMP NOT NULL, shipping_date TIMESTAMP, payment_amount INTEGER NOT NULL, card_no VARCHAR NOT NULL);
-INSERT INTO transactions (order_id, payment_date, shipping_date, payment_amount, card_no) VALUES(1, '2022-01-01', '2022-01-01', 140, '1234');
-INSERT INTO transactions (order_id, payment_date, shipping_date, payment_amount, card_no) VALUES(1, '2022-01-02', NULL, 0, '1234');
+CREATE TABLE transaction_status (id SERIAL UNIQUE, name VARCHAR NOT NULL);
+INSERT INTO transaction_status (id, name) VALUES(1, 'pending payment'), (2, 'pending shipping'), (3, 'shipping'), (4, 'received'), (5, 'canceled');
 
-CREATE TABLE IF NOT EXISTS models (name VARCHAR UNIQUE NOT NULL, "desc" VARCHAR NOT NULL);
+CREATE TABLE transactions (id SERIAL UNIQUE, order_id INTEGER REFERENCES orders(id), payment_date TIMESTAMP NOT NULL, shipping_date TIMESTAMP, payment_amount INTEGER NOT NULL, card_no VARCHAR NOT NULL, status INTEGER NOT NULL REFERENCES transaction_status(id));
+INSERT INTO transactions (order_id, payment_date, shipping_date, payment_amount, card_no, status) VALUES(1, '2022-01-01', '2022-01-01', 140, '1234', 4);
+INSERT INTO transactions (order_id, payment_date, shipping_date, payment_amount, card_no, status) VALUES(1, '2022-01-02', NULL, 0, '1234', 1);
 
 INSERT INTO models VALUES('Item', 'description');
 INSERT INTO models VALUES('Customer', 'description');
 INSERT INTO models VALUES('Order', 'description');
-INSERT INTO models VALUES('Payment', 'description');
+INSERT INTO models VALUES('PaidOrder', 'description');
 INSERT INTO models VALUES('Transaction', 'description');
+INSERT INTO models VALUES('ShippingOrder', 'description');
 INSERT INTO models VALUES('FullfilledOrder', 'description');
-
-CREATE TABLE IF NOT EXISTS datamodel (name VARCHAR NOT NULL REFERENCES models(name), "table" VARCHAR NOT NULL, field VARCHAR NOT NULL, PRIMARY KEY (name, "table", field));
+INSERT INTO models VALUES('CanceledOrder', 'description');
 
 INSERT INTO datamodel VALUES('Item', 'items', 'id');
 INSERT INTO datamodel VALUES('Item', 'items', 'name');
@@ -43,17 +44,29 @@ INSERT INTO datamodel VALUES('Order', 'orders', 'id');
 INSERT INTO datamodel VALUES('Order', 'orders', 'customer_id');
 INSERT INTO datamodel VALUES('Order', 'orders', 'total_price');
 
-INSERT INTO datamodel VALUES('Payment', 'transaction', 'order_id');
-INSERT INTO datamodel VALUES('Payment', 'transaction', 'payment_date');
-INSERT INTO datamodel VALUES('Payment', 'transaction', 'payment_amount');
-INSERT INTO datamodel VALUES('Payment', 'transaction', 'card_no');
+INSERT INTO datamodel VALUES('PaidOrder', 'transaction', 'id');
+INSERT INTO datamodel VALUES('PaidOrder', 'transaction', 'order_id');
+INSERT INTO datamodel VALUES('PaidOrder', 'transaction', 'payment_date');
+INSERT INTO datamodel VALUES('PaidOrder', 'transaction', 'payment_amount');
+INSERT INTO datamodel VALUES('PaidOrder', 'transaction', 'card_no');
+INSERT INTO datamodel VALUES('PaidOrder', 'transaction', 'status');
 
-CREATE TABLE templates (id SERIAL UNIQUE, name VARCHAR NOT NULL, created TIMESTAMP NOT NULL, updated TIMESTAMP NOT NULL, process_name VARCHAR NOT NULL);
+INSERT INTO datamodel VALUES('ShippingOrder', 'transaction', 'id');
+INSERT INTO datamodel VALUES('ShippingOrder', 'transaction', 'order_id');
+INSERT INTO datamodel VALUES('ShippingOrder', 'transaction', 'payment_date');
+INSERT INTO datamodel VALUES('ShippingOrder', 'transaction', 'payment_amount');
+INSERT INTO datamodel VALUES('ShippingOrder', 'transaction', 'shipping_date');
+INSERT INTO datamodel VALUES('ShippingOrder', 'transaction', 'card_no');
+INSERT INTO datamodel VALUES('ShippingOrder', 'transaction', 'status');
 
-CREATE TABLE components (id SERIAL UNIQUE, template_id INTEGER REFERENCES templates(id) ON DELETE CASCADE NOT NULL, input_dep VARCHAR, input_dep_field VARCHAR, output_dep VARCHAR, output_dep_field VARCHAR, "order" INTEGER NOT NULL, "name" VARCHAR NOT NULL, type VARCHAR NOT NULL, required BOOLEAN DEFAULT true, hide BOOLEAN DEFAULT false, validation VARCHAR, function VARCHAR, parent INTEGER REFERENCES components(id) ON DELETE CASCADE);
+INSERT INTO datamodel VALUES('CanceledOrder', 'transaction', 'id');
+INSERT INTO datamodel VALUES('CanceledOrder', 'transaction', 'order_id');
+INSERT INTO datamodel VALUES('CanceledOrder', 'transaction', 'status');
 
-CREATE TABLE input_information_parent (id SERIAL UNIQUE, name VARCHAR NOT NULL, "order" INTEGER NOT NULL, input_dep VARCHAR REFERENCES models(name), template_id INTEGER NOT NULL REFERENCES templates(id) ON DELETE CASCADE);
-
-CREATE TABLE input_information_child (id SERIAL UNIQUE, name VARCHAR NOT NULL, "order" INTEGER NOT NULL, input_dep_field VARCHAR, hide BOOLEAN DEFAULT false, parent_id INTEGER NOT NULL REFERENCES input_information_parent(id) ON DELETE CASCADE);
-
-CREATE TABLE input_information_child_query (id SERIAL UNIQUE, foreign_table VARCHAR NOT NULL, foreign_key VARCHAR NOT NULL, query_table VARCHAR NOT NULL, query_field VARCHAR NOT NULL, details_id INTEGER NOT NULL REFERENCES input_information_child(id) ON DELETE CASCADE);
+INSERT INTO datamodel VALUES('FullfilledOrder', 'transaction', 'id');
+INSERT INTO datamodel VALUES('FullfilledOrder', 'transaction', 'order_id');
+INSERT INTO datamodel VALUES('FullfilledOrder', 'transaction', 'payment_date');
+INSERT INTO datamodel VALUES('FullfilledOrder', 'transaction', 'payment_amount');
+INSERT INTO datamodel VALUES('FullfilledOrder', 'transaction', 'shipping_date');
+INSERT INTO datamodel VALUES('FullfilledOrder', 'transaction', 'card_no');
+INSERT INTO datamodel VALUES('FullfilledOrder', 'transaction', 'status');
